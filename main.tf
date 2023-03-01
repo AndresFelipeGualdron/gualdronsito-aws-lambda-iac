ce6f9fa5343ecab8c056478fc13a801af6d7baa7
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 17:58:26 -0500

    fix

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = var.http_method_integration
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
