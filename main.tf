03b26776af9892ba9fcbe19a2257f3cb20cc2105
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 18:50:16 -0500

    fix

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_api_gateway_method" "method" {
 resource "aws_api_gateway_method_response" "method_response" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id   = aws_api_gateway_resource.resource.id
-  http_method = ws_api_gateway_method.method.http_method
+  http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
 }
 
