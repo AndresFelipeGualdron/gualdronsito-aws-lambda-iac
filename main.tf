1a02002838388e3ed3bcd7aa3126cf91597dfe52
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-02-28 19:34:40 -0500

    init project

---
+++ main.tf
@@ -0,0 +1,67 @@
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "api" {
+  name = var.api_name
+  body = var.body
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  path_part   = "resource"
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "arn:aws:execute-api:${var.myregion}:${var.accountId}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.resource.path}"
+}
+
+resource "aws_lambda_function" "lambda" {
+  filename      = "components/lambda-template/template.zip"
+  function_name = var.lambda_name
+  role          = aws_iam_role.role.arn
+  handler       = var.lambda_handler
+  runtime       = var.lambda_runtime
+
+  source_code_hash = filebase64sha256("components/lambda-template/template.zip")
+}
+
+# IAM
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = var.name_role
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "myregion1" {
+  type = string
+}
+
+variable "accountId" {
+  type = string
+}
+
+variable "api_name" {
+  type = string
+}
+
+variable "body" {}
+
+variable "lambda_name" {
+  type = string
+}
+
+variable "lambda_handler" {
+  type = string
+}
+
+variable "lambda_runtime" {
+  type = string
+}
+
+variable "path_lambdazip" {
+  type = string
+}
+
+variable "name_role" {
+  
+}
