39ec8c3153cbbfd5e880ed1eaffb229e23e6eb56
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 16:18:26 -0500

    fix

--- main.tf
+++ main.tf
@@ -85,6 +85,15 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = aws_lambda_function.lambda.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "integration_options" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method_options.http_method
+  integration_http_method = "POST"
+  type                    = "MOCK"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
 # Lambda
 resource "aws_lambda_permission" "apigw_lambda" {
   statement_id  = "AllowExecutionFromAPIGateway"
