7f2b405b10d6daee724ce72c65c437be0669bb42
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 16:23:34 -0500

    fix

--- main.tf
+++ main.tf
@@ -63,19 +63,6 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   }
 }
 
-resource "aws_api_gateway_integration_response" "integration_response_options" {
-  rest_api_id = aws_api_gateway_rest_api.api.id
-  resource_id = aws_api_gateway_resource.resource.id
-  http_method = aws_api_gateway_method.method_options.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-  }
-}
-
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
