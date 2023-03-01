06d0d6271d956128ccdd507fad7086d66ad22d84
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 18:45:37 -0500

    fix

--- main.tf
+++ main.tf
@@ -18,6 +18,13 @@ resource "aws_api_gateway_method" "method" {
   api_key_required = false
 }
 
+resource "aws_api_gateway_method_response" "method_response" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method = ws_api_gateway_method.method.http_method
+  status_code = "200"
+}
+
 resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
