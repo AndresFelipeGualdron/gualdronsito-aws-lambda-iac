7e732a36ba3c4c66f03e8958976e7f884892043f
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 18:16:08 -0500

    fix

--- main.tf
+++ main.tf
@@ -22,9 +22,18 @@ resource "aws_api_gateway_integration" "integration" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method.http_method
-  integration_http_method = "POST"
+  integration_http_method = var.http_method_integration
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.lambda.invoke_arn
+  passthrough_behavior = "WHEN_NO_TEMPLATES"
+  request_templates = {
+    "application/json" = jsonencode({
+      statusCode = "200",
+      headers    = {
+        "Access-Control-Allow-Origin" = "'*'"
+      }
+    })
+  }
 }
 
 # Lambda
