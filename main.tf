76def6a9ee6d4dc77b0326a64ba8e03dc337815c
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-01 19:42:06 -0500

    fix

--- main.tf
+++ main.tf
@@ -23,6 +23,10 @@ resource "aws_api_gateway_method_response" "method_response" {
   resource_id = aws_api_gateway_resource.resource.id
   http_method = aws_api_gateway_method.method.http_method
   status_code = "200"
+
+  response_models = {
+    
+  }
 }
 
 resource "aws_api_gateway_integration_response" "response_integration" {
