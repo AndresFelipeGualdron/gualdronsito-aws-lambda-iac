462aec4379429e4455a2eae77e182ccd3afa9088
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-02 11:20:48 -0500

    fix

--- main.tf
+++ main.tf
@@ -97,7 +97,6 @@ resource "aws_api_gateway_integration" "integration_options" {
   rest_api_id             = aws_api_gateway_rest_api.api.id
   resource_id             = aws_api_gateway_resource.resource.id
   http_method             = aws_api_gateway_method.method_options.http_method
-  integration_http_method = "OPTIONS"
   type                    = "MOCK"
 }
 
