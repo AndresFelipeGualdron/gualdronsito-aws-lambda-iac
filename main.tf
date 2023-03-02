eec6e09bae423e91c95d89a50cea75eebd185633
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-02 10:26:38 -0500

    fix

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ resource "aws_api_gateway_method_response" "method_response" {
 }
 
 resource "aws_api_gateway_integration_response" "response_integration" {
+  id = "id_response_integration"
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.resource.id
   http_method = aws_api_gateway_method.method.http_method
