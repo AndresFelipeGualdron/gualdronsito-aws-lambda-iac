2530b0cdd1fece2e645ad5791511b2f8a5ec6d05
AndresFelipeGualdron <gualdronsito@hotmail.com>
2023-03-02 11:04:55 -0500

    fix

--- main.tf
+++ main.tf
@@ -61,7 +61,8 @@ resource "aws_api_gateway_integration_response" "response_integration" {
   status_code = "200"
 
   depends_on = [
-    aws_api_gateway_rest_api.api
+    aws_api_gateway_rest_api.api,
+    aws_api_gateway_resource.resource,
   ]
 
 }
@@ -73,7 +74,8 @@ resource "aws_api_gateway_integration_response" "response_integration_options" {
   status_code = "200"
 
   depends_on = [
-    aws_api_gateway_rest_api.api
+    aws_api_gateway_rest_api.api,
+    aws_api_gateway_resource.resource,
   ]
 
   response_parameters = {
